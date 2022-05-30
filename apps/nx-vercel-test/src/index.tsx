0b3dad203b594d6f9f8f5acb9a1b582be5bf739b
AdrianBlancoGarcia <adrian@open-bootcamp.com>
2022-05-30 16:41:38 +0200

    hot-fix: release

--- apps/nx-vercel-test/src/index.tsx
+++ apps/nx-vercel-test/src/index.tsx
@@ -9,11 +9,13 @@ import App from './App';
 
 import './index.css';
 
+const RELEASE = process.env.npm_package_version
+
 Sentry.init({
   dsn: "https://2990a5cb084440a0836dbe2da3c21a22@o1267595.ingest.sentry.io/6454133",
   integrations: [new BrowserTracing()],
   debug: true,
-  release: "nx-vercel-test@" + process.env.npm_package_version,
+  release: "nx-vercel-test@" + RELEASE,
   tracesSampleRate: 1.0,
 });
 
