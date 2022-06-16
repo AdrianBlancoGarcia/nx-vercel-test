dd521fdc759f7750756524b6e9d3a7719b691958
AdrianBlancoGarcia <adrian@open-bootcamp.com>
2022-06-16 12:11:09 +0200

    feat: sentry change

--- apps/nx-vercel-test/src/index.tsx
+++ apps/nx-vercel-test/src/index.tsx
@@ -12,7 +12,7 @@ import './index.css';
 const RELEASE = process.env.npm_package_version
 
 Sentry.init({
-  dsn: "http://6e7c680d648941ac9df6d6e7e92e6dc9@2ac4-139-47-24-207.ngrok.io",
+  dsn: "http://b65310dd0ebd49afaa65bccd0a9ef4ba@@2ac4-139-47-24-207.ngrok.io",
   integrations: [new BrowserTracing()],
   debug: true,
   release: "nx-vercel-test@1.1.2",
